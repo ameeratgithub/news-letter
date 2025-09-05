02d8c5adb0f40ee7346453fbc30355aa0a1262dd
Ameer Hamza <ameeratwork@gmail.com>
2025-09-05 17:48:24 +0500

    refactor: bind to random ports for concurrent tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 //! src/main.rs
 
+use std::net::TcpListener;
+
 use news_letter::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Can't bind to port 8000");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 //! tests/health_check.rs
 
+use std::net::TcpListener;
+
 /// `tokio::test` is the testing equivalent of `tokio::main`.
 /// It also spares you from having to specify the `#[test]` attribute.
 ///
@@ -9,14 +11,14 @@
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest` to perform HTTP requests against
     // our application
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{address}/health_check"))
         .send()
         .await
         .expect("Failed to execute request");
@@ -27,8 +29,13 @@ async fn health_check_works() {
 }
 
 // In tests, it's not worth to propagate errors. Panic and crash if server setup fails
-fn spawn_app() {
-    let server = news_letter::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Get the port number given to us by the operating system
+    let port = listener.local_addr().unwrap().port();
+    let server = news_letter::run(listener).expect("Failed to bind address");
     // Launch the server as a background task.
     let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
 }
