609540afd7b53c0cadd1f32710b61bd0d1a7815b
Ameer Hamza <ameeratwork@gmail.com>
2025-09-14 22:05:15 +0500

    deploy: environment variables and database configuration added

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use news_letter::{
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 #[tokio::main]
@@ -16,9 +15,8 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Will try to establish a connection when pool is used for first time
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to postgres");
+    let connection_pool = PgPoolOptions::new()
+        .connect_lazy_with(configuration.database.with_db());
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
