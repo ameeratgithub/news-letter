cdf19120953f262a9c0c6db5a7db721e13cab004
Ameer Hamza <ameeratwork@gmail.com>
2025-09-17 04:40:48 +0500

    feat: email client, to send emails via a service provider, added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use news_letter::{
     configuration::get_configuration,
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -16,10 +17,23 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Will try to establish a connection when pool is used for first time
     let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address");
+    let timeout=configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+        timeout,
+    );
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
