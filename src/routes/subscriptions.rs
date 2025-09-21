c34e20082b7963c8f224a8c3f5960362e51fa1b4
Ameer Hamza <ameeratwork@gmail.com>
2025-09-21 07:17:39 +0500

    refactor: useless comments removed

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -79,16 +79,6 @@ pub enum SubscribeError {
     ValidationError(String),
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
-    // #[error("Failed to acquire a Postgres connection from the pool")]
-    // PoolError(#[source] sqlx::Error),
-    // #[error("Failed to insert new subscriber in the database")]
-    // InsertSubscriberError(#[source] sqlx::Error),
-    // #[error("Failed to store the confirmation token for a new subscriber")]
-    // StoreTokenError(#[from] StoreTokenError),
-    // #[error("Failed to commit SQL transaction to store a new subscriber")]
-    // TransactionCommitError(#[source] sqlx::Error),
-    // #[error("Failed to send a confirmation email")]
-    // SendEmailError(#[from] reqwest::Error),
 }
 
 impl std::fmt::Debug for SubscribeError {
@@ -102,11 +92,6 @@ impl ResponseError for SubscribeError {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
             SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            // SubscribeError::PoolError(_)
-            // | SubscribeError::TransactionCommitError(_)
-            // | SubscribeError::InsertSubscriberError(_)
-            // | SubscribeError::StoreTokenError(_)
-            // | SubscribeError::SendEmailError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
